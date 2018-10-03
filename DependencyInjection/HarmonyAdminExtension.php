96a22815633309978570cfcf56dce2282f86a9ac
David Sanchez <david.sanchez@nventive.com>
2018-10-03 12:33:59 -0400

    Load easyadmin configuration after **EasyAdminBundle** is loaded

--- DependencyInjection/HarmonyAdminExtension.php
+++ DependencyInjection/HarmonyAdminExtension.php
@@ -5,6 +5,7 @@ namespace Harmony\Bundle\AdminBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
@@ -12,7 +13,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
  *
  * @package Harmony\Bundle\AdminBundle\DependencyInjection
  */
-class HarmonyAdminExtension extends Extension
+class HarmonyAdminExtension extends Extension implements PrependExtensionInterface
 {
 
     /**
@@ -20,12 +21,27 @@ class HarmonyAdminExtension extends Extension
      *
      * @param array            $configs
      * @param ContainerBuilder $container
-     *
-     * @throws \Exception
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
-        $loader->load('easyadmin.yaml');
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param ContainerBuilder $container
+     *
+     * @throws \Exception
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        // get all bundles
+        $bundles = $container->getParameter('kernel.bundles');
+
+        // determine if EasyAdminBundle is registered before loading configuration
+        if (isset($bundles['EasyAdminBundle'])) {
+            $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
+            $loader->load('easyadmin.yaml');
+        }
     }
 }

297117a2a2b9b1f0ec6d6b8bb1c1a94094ba2dd0
David Sanchez <david.sanchez@nventive.com>
2018-10-03 12:03:22 -0400

    Add main classes and dependencies

---
+++ DependencyInjection/HarmonyAdminExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Harmony\Bundle\AdminBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * Class HarmonyAdminExtension
+ *
+ * @package Harmony\Bundle\AdminBundle\DependencyInjection
+ */
+class HarmonyAdminExtension extends Extension
+{
+
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array            $configs
+     * @param ContainerBuilder $container
+     *
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
+        $loader->load('easyadmin.yaml');
+    }
+}
---
+++ HarmonyAdminBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Harmony\Bundle\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class HarmonyAdminBundle
+ *
+ * @package Harmony\Bundle\AdminBundle
+ */
+class HarmonyAdminBundle extends Bundle
+{
+
+}

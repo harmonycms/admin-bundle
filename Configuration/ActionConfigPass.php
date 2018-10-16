110effb0341722405128e5813667c8fdfcb74e6a
David Sanchez <david.sanchez@nventive.com>
2018-10-16 15:46:54 -0400

    Update button classes actions for `edit` and `delete`

--- Configuration/ActionConfigPass.php
+++ Configuration/ActionConfigPass.php
@@ -254,21 +254,29 @@ class ActionConfigPass implements ConfigPassInterface
     private function getDefaultActionsConfig($view)
     {
         $actions = $this->doNormalizeActionsConfig([
-            'delete' => ['name' => 'delete', 'label' => 'action.delete', 'icon' => 'trash-o', 'css_class' => 'btn btn-default'],
-            'edit' => ['name' => 'edit', 'label' => 'action.edit', 'icon' => 'edit', 'css_class' => 'btn btn-primary'],
-            'new' => ['name' => 'new', 'label' => 'action.new', 'css_class' => 'btn btn-primary'],
+            'delete' => ['name'      => 'delete',
+                         'label'     => 'action.delete',
+                         'icon'      => 'trash-o',
+                         'css_class' => 'btn btn-default'
+            ],
+            'edit'   => ['name'      => 'edit',
+                         'label'     => 'action.edit',
+                         'icon'      => 'edit',
+                         'css_class' => 'btn btn-primary'
+            ],
+            'new'    => ['name' => 'new', 'label' => 'action.new', 'css_class' => 'btn btn-primary'],
             'search' => ['name' => 'search', 'label' => 'action.search'],
-            'show' => ['name' => 'show', 'label' => 'action.show'],
-            'list' => ['name' => 'list', 'label' => 'action.list', 'css_class' => 'btn btn-secondary'],
+            'show'   => ['name' => 'show', 'label' => 'action.show'],
+            'list'   => ['name' => 'list', 'label' => 'action.list', 'css_class' => 'btn btn-secondary'],
         ]);
 
         // minor tweaks for some action + view combinations
         if ('list' === $view) {
-            $actions['delete']['icon'] = null;
-            $actions['delete']['css_class'] = 'text-danger';
-            $actions['edit']['icon'] = null;
-            $actions['edit']['css_class'] = 'text-primary';
-            $actions['list']['css_class'] = '';
+            $actions['delete']['icon']      = null;
+            $actions['delete']['css_class'] = 'btn btn-danger';
+            $actions['edit']['icon']        = null;
+            $actions['edit']['css_class']   = 'btn btn-primary';
+            $actions['list']['css_class']   = '';
         }
 
         return $actions;

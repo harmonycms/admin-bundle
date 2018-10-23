93468c13e94d305aacebdeb42a74bb3524d6028f
David Sanchez <david.sanchez@nventive.com>
2018-10-23 12:05:53 -0400

    Update code to save user and display success flash message

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace Harmony\Bundle\AdminBundle\Controller;
 
+use FOS\UserBundle\Doctrine\UserManager;
 use Harmony\Bundle\AdminBundle\Form\Type\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,8 +29,16 @@ class UserController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $user          = $form->getData();
             $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->flush($form->getData());
+            /** @var UserManager $userManager */
+            $userManager = $this->get('fos_user.user_manager');
+            $userManager->updatePassword($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'flash.profile_success');
+
+            return $this->redirectToRoute('admin_profile');
         }
 
         return $this->render('@HarmonyAdmin/default/profile.html.twig', [

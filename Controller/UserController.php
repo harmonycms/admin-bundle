ff76e12443670698990d951f6d861321baf4c627
David Sanchez <david38sanchez@gmail.com>
2018-10-22 23:16:36 -0400

    Add code for profile, with form and translations

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -2,7 +2,9 @@
 
 namespace Harmony\Bundle\AdminBundle\Controller;
 
+use Harmony\Bundle\AdminBundle\Form\Type\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,10 +18,23 @@ class UserController extends Controller
 
     /**
      * @Route("/profile", name="admin_profile")
+     * @param Request $request
+     *
      * @return Response
      */
-    public function profile(): Response
+    public function profile(Request $request): Response
     {
-        return $this->render('@HarmonyAdmin/default/profile.html.twig');
+        $form = $this->createForm(UserType::class, $this->getUser());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush($form->getData());
+        }
+
+        return $this->render('@HarmonyAdmin/default/profile.html.twig', [
+            'user' => $this->getUser(),
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ Form/Type/UserType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Harmony\Bundle\AdminBundle\Form\Type;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class UserType
+ *
+ * @package Harmony\Bundle\AdminBundle\Form\Type
+ */
+class UserType extends AbstractType
+{
+
+    /**
+     * Builds the form.
+     * This method is called for each type in the hierarchy starting from the
+     * top most type. Type extensions can further modify the form.
+     *
+     * @see FormTypeExtensionInterface::buildForm()
+     *
+     * @param FormBuilderInterface $builder The form builder
+     * @param array                $options The options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('username', TextType::class, ['label' => 'form.username'])
+            ->add('email', EmailType::class, ['label' => 'form.email'])
+            ->add('password', RepeatedType::class, [
+                'type'            => PasswordType::class,
+                'invalid_message' => 'form.password_invalid',
+                'required'        => false,
+                'first_options'   => ['label' => 'form.new_password'],
+                'second_options'  => ['label' => 'form.new_password_confirmation']
+            ])
+            ->add('save', SubmitType::class, ['label' => 'action.save']);
+    }
+
+    /**
+     * Configures the options for this type.
+     *
+     * @param OptionsResolver $resolver The resolver for the options
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(['data_class' => User::class, 'translation_domain' => 'HarmonyAdminBundle']);
+    }
+}

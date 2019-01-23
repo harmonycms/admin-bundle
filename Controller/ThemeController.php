d12aecf292db37bd82d1f71f1d5af54d782d1760
David Sanchez <david.sanchez@nventive.com>
2019-01-23 16:25:51 -0500

    Add empty theme manager

---
+++ Controller/ThemeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Harmony\Bundle\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Class ThemeController
+ *
+ * @package Harmony\Bundle\AdminBundle\Controller
+ */
+class ThemeController extends AbstractController
+{
+
+}

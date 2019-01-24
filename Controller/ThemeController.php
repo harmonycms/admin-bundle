1eb451338065150d5cce48608066e48d56b4edc6
David Sanchez <david38sanchez@gmail.com>
2019-01-23 21:41:56 -0500

    Add `index` action to list themes

--- Controller/ThemeController.php
+++ Controller/ThemeController.php
@@ -2,14 +2,38 @@
 
 namespace Harmony\Bundle\AdminBundle\Controller;
 
+use Harmony\Bundle\ThemeBundle\Locator\ThemeLocator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class ThemeController
+ * @Route("/theme", name="admin_theme_")
  *
  * @package Harmony\Bundle\AdminBundle\Controller
  */
 class ThemeController extends AbstractController
 {
 
+    /** @var ThemeLocator $themeLocator */
+    protected $themeLocator;
+
+    /**
+     * ThemeController constructor.
+     *
+     * @param ThemeLocator $themeLocator
+     */
+    public function __construct(ThemeLocator $themeLocator)
+    {
+        $this->themeLocator = $themeLocator;
+    }
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('@HarmonyAdmin\theme\index.html.twig', ['themes' => $this->themeLocator->getThemeData()]);
+    }
 }

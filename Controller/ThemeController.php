3ceda72a092056d2304a0c2138c6cd0984f4ff53
David Sanchez <david.sanchez@nventive.com>
2019-02-11 11:13:37 -0500

    Update code to get list of themes

--- Controller/ThemeController.php
+++ Controller/ThemeController.php
@@ -3,8 +3,8 @@
 namespace Harmony\Bundle\AdminBundle\Controller;
 
 use Harmony\Bundle\CoreBundle\Manager\SettingsManager;
-use Harmony\Bundle\ThemeBundle\Locator\ThemeLocator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,34 +17,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class ThemeController extends AbstractController
 {
 
-    /** @var ThemeLocator $themeLocator */
-    protected $themeLocator;
-
     /** @var SettingsManager $settingsManager */
     protected $settingsManager;
+
+    /** @var ParameterBagInterface $parameterBag */
+    protected $parameterBag;
 
     /**
      * ThemeController constructor.
      *
-     * @param ThemeLocator    $themeLocator
-     * @param SettingsManager $settingsManager
+     * @param SettingsManager       $settingsManager
+     * @param ParameterBagInterface $parameterBag
      */
-    public function __construct(ThemeLocator $themeLocator, SettingsManager $settingsManager)
+    public function __construct(SettingsManager $settingsManager, ParameterBagInterface $parameterBag)
     {
-        $this->themeLocator    = $themeLocator;
         $this->settingsManager = $settingsManager;
+        $this->parameterBag    = $parameterBag;
     }
 
     /**
      * @Route("/", name="index")
      * @return Response
-     * @throws \Harmony\Bundle\ThemeBundle\Json\JsonValidationException
-     * @throws \Seld\JsonLint\ParsingException
      */
     public function index(): Response
     {
-        return $this->render('@HarmonyAdmin\theme\index.html.twig',
-            ['themes' => $this->themeLocator->discoverThemes()]);
+        $themes = [];
+        foreach ($this->parameterBag->get('kernel.themes') as $name => $theme) {
+            // TODO: retrieve object from Kernel without creating a new one
+            $themes[$name] = new $theme;
+        }
+
+        return $this->render('@HarmonyAdmin\theme\index.html.twig', ['themes' => $themes]);
     }
 
     /**

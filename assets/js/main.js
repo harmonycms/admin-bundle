b2b30bec096f87e088786f2b72ce48873672c476
David Sanchez <david38sanchez@gmail.com>
2019-02-21 19:54:04 -0500

    Add new dependency `datatable`

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
 // any CSS you require will output into a single css file (main.css in this case)
 import '../sass/main.scss';
-
 // Bootstrap
 import 'bootstrap';
-
 // MetisMenu
 import 'metismenu';
+// DataTable
+import 'datatables';
 
 $(function () {
   $(".metismenu").metisMenu();
+
+  $('[data-toggle="dataTable"]').dataTable();
 });

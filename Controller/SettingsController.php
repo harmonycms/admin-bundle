b9e8e155824134468fe951a93bebe3d8eb0d47b6
David Sanchez <david.sanchez@nventive.com>
2019-05-23 14:55:21 -0400

    Add code to list all parameters from database

---
+++ Controller/SettingsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Harmony\Bundle\AdminBundle\Controller;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Harmony\Bundle\CoreBundle\Model\Parameter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class SettingsController
+ * @Route("/settings", name="admin_settings_")
+ *
+ * @package Harmony\Bundle\AdminBundle\Controller
+ */
+class SettingsController extends AbstractController
+{
+
+    /** @var ManagerRegistry $registry */
+    protected $registry;
+
+    /**
+     * SettingsController constructor.
+     *
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    /**
+     * @Route("/parameters", name="parameters")
+     * @return Response
+     */
+    public function parameters(): Response
+    {
+        return $this->render('@HarmonyAdmin\settings\parameters.html.twig', [
+            'parameters' => $this->registry->getRepository(Parameter::class)->findAll()
+        ]);
+    }
+}
